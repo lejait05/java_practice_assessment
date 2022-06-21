6d5b5945c5cbe5f885fa9eaf87fcf7c0292ec0b1
Lineisha <lineishagibson@gmail.com>
2022-06-20 22:12:08 -0500

    practice

---
+++ src/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    public static void main(String[] args) {
+        int num;
+        num = 40;
+        int subtractTen = num - 10;
+        System.out.println(subtractTen);
+        average =
+    }
+}
